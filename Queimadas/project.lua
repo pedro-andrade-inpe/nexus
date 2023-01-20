9d5d7080045c7a7f7c1d52bd544d294336c1481e
Luis Henrique da Silva Campos <luis.henrique_campos@outlook.com.br>
2023-01-20 18:47:40 -0300

    alter folder 'Campinas Grande' for 'Queimadas'

--- Queimadas/project.lua
+++ Queimadas/project.lua
@@ -4,7 +4,7 @@ import("gis")
 project = Project{
 	title = "Região de Queimadas",
 	author = "NEXUS",
-	file = "campina-g.tview",
+	file = "queimadas.tview",
 	clean = true,
 	trajectory = "trajectory_campina_grande_invert.shp",
 	points = "Pontos_campo_solos_abril_comunicacao.shp"
