6ac7e86790c4b24437f381c91fcec80bd99eca27
Luis Henrique da Silva Campos <luis.henrique_campos@outlook.com.br>
2023-05-10 17:36:00 -0300

    small changes

--- Fitofisionomias/project.lua
+++ Fitofisionomias/project.lua
@@ -6,7 +6,7 @@ Project{
   file = "fitofisionomias.tview", 
 	author = "NEXUS",
 	clean = true,
-	limit = "mergedpolly.shp",
-	points = "dot-0001.shp",
+	limite = "mergedpolly.shp",
+	pontos = "dot-0001.shp",
 }
 
