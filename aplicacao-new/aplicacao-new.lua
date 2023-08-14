21b25b66bdabe7f5f49b4860ed790678d4298a1e
Luis Henrique da Silva Campos <luis.henrique_campos@outlook.com.br>
2023-08-14 10:56:44 -0300

    small updates

--- aplicacao-new/aplicacao-new.lua
+++ aplicacao-new/aplicacao-new.lua
@@ -20,7 +20,7 @@ Project {
 
 
 Application {
-	-- key = "AIzaSyA1coAth-Bo7m99rnxOm2oOBB88AmaSbOk",
+	key = "AIzaSyA1coAth-Bo7m99rnxOm2oOBB88AmaSbOk",
 	project = "Projeto.tview",
 	base = "roadmap",
 	title = "Nexus - Caminhos para a sustentabilidade",
