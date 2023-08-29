7e57484b11939c08b3256a80d3d4e4230c4eca28
Luis Henrique da Silva Campos <luis.henrique_campos@outlook.com.br>
2023-08-29 18:31:53 -0300

    Small changes

--- oficinas/application.lua
+++ oficinas/application.lua
@@ -2,8 +2,18 @@ import("gis")
 import("publish")
 
 
-local description = [[
-  O projeto NEXUS prevê a adoção de um processo em multi-escala. Além da escala regional (Cerrado, Caatinga e Bacias do São Francisco e Paranaiba), foram selecionadas 3 escalas locais. Foi realizado um processo quantitativo, baseado em indicadores e viagens de campo exploratórias no qual um conjunto inicial de municípios foi selecionado. Estes municípios se caracterizam por apresentarem uma problemática relacionada ao tema central do projeto: Nexus água-agricultura-energia.
+description = [[
+  O projeto NEXUS prevê a adoção de um processo em multi-escala. Além da escala regional (Cerrado, Caatinga e Bacias do São Francisco e Paranaiba), foram selecionadas 3 escalas locais. Foi realizado um processo quantitativo, baseado em indicadores e viagens de campo exploratórias no qual um conjunto inicial de municípios foi selecionado. Estes municípios se caracterizam por apresentarem uma problemática relacionada ao tema central do projeto: Nexus água-agricultura-energia.<br>
+
+  <p>O processo de preparação para as oficinas locais (incluindo o mapeamento e seleção de participantes para as oficinas) foi realizada através da adaptação da ferramenta chamada  <b>Net-Map</b> <b>(SATTLER; 2022)</b><br></p>
+  <p>O <b>Net-Map</b>, em Inglês ou Mapa de Rede, em Português, é uma ferramenta de mapeamento baseada em entrevistas que ajuda as pessoas a entenderem, visualizarem e discutirem situações em que muitos atores diferentes influenciam o sistema em questão. Ao criar um Mapa de Rede de Influência, indivíduos e grupos podem esclarecer suas próprias visões sobre uma situação, promover discussões e desenvolver uma abordagem estratégica para suas atividades de rede.<br></p> Mais especificamente, o Mapa de Rede ajuda os participantes a determinar:<br>
+  • quais atores estão envolvidos em uma determinada rede;<br>
+  • como eles estão vinculados;<br>
+  • quão influentes eles são.<br><br>
+
+
+  <b>REFERÊNCIA:</b><br>
+  <b>SATTLER</b>, Claudia. Using Process Net-Map to analyze governance innovations in the forestry sector. Ecosystem Services, v. 55, p. 101433, 2022.
 ]]
 
 Project {
@@ -15,7 +25,7 @@ Project {
 }
 
 Application {
-  key = "AIzaSyA1coAth-Bo7m99rnxOm2oOBB88AmaSbOk",
+  -- key = "AIzaSyA1coAth-Bo7m99rnxOm2oOBB88AmaSbOk",
   project = "project.tview",
   base = "roadmap",
   description = description,
@@ -27,7 +37,7 @@ Application {
 
 
   Points = View {
-    description = "inserir descrição",
+    description = "Oficinas participativas",
     select = "Local",
     icon = "flag",
     label = "locais",
@@ -53,12 +63,6 @@ Application {
       Interesse = {
         class = "bar",
         id = "1",
-        beginAtZero = true,
-
-        -- titile = [[Atores por setor de interesse na Escala local]],
-        
-       
-            
         columns = {
           "Setor Privado",
           "Participantes do Governo",
@@ -80,6 +84,8 @@ Application {
       }
 
       if cell.Local == "Campina-Grande" then
+        report:addText
+          "Campina Grande-PB e municípios ao entorno, foram escolhidos por fazerem parte da área de transposição do Rio São Francisco na região mais seca do semiárido."
         report:addText(text)
         report:addGraphic(Interesse)
         report:addText(Descricao)
@@ -87,7 +93,9 @@ Application {
         report:addImage("CampinaGrande1.jpg")
         report:addImage("CampinaGrande2.jpg")
       elseif cell.Local == "Barreiras" then
-        report:addText(text)
+        report:addText
+          "Barreiras e municípios vizinhos, localizados no Oeste da Bahia e parte da região MATOPIBA,foram escolhidos por fazerem parte de uma região crítica no contexto da produção de commodities agrícolas e irrigação em larga escala."
+          report:addText(text)
         report:addGraphic(Interesse)
         report:addText(Descricao)
         report:addSeparator()
@@ -95,6 +103,8 @@ Application {
         report:addImage("Barreiras2.jpg")
         report:addImage("Barreiras3.jpg")
       else
+        report:addText
+        "Petrolina/Juazeiro e municípios ao entorno por estarem ligados ao pólo de produção de frutas irrigadas as margens do Rio São Francisco, onde também estão localizadas hidrelétricas de grande porte."
         report:addText(text)
         report:addGraphic(Interesse)
         report:addText(Descricao)
